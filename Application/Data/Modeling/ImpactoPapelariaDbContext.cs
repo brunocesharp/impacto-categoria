2dda4560fe722593152455f2f3086fdfa6544a8d
Bruno Alves Siqueira <alvesSiqueira@outlook.com>
2023-11-16 19:32:03 -0300

    first commit

---
+++ Application/Data/Modeling/ImpactoPapelariaDbContext.cs
@@ -0,0 +1,42 @@
+﻿using ImpactoManager.Application.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImpactoManager.Application.Data.Modeling
+{
+    public class ImpactoPapelariaDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public ImpactoPapelariaDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ServerVersion.AutoDetect(_configuration.GetConnectionString("ImpactoDb")));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SetCategoriaTable(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static void SetCategoriaTable(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>()
+                        .ToTable("Categoria")
+                        .Property(x => x.Codigo)
+                        .HasColumnName("Codigo");
+
+            modelBuilder.Entity<Categoria>()
+                        .ToTable("Categoria")
+                        .Property(x => x.Descricao)
+                        .HasColumnName("Descricao");
+
+            modelBuilder.Entity<Categoria>().HasKey(x => x.Codigo);
+        }
+    }
+}
---
+++ Application/Data/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace ImpactoManager.Application.Data.Models
+{
+    public class Categoria
+    {
+        public int? Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
